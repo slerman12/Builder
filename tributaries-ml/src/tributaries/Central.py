2d4e432869b76c62b994919dc4e1cb7c350e0839
sam <slerman@u.rochester.edu>
2023-07-04 04:02:07 -0400

    tributaries launching

--- tributaries-ml/src/tributaries/Central.py
+++ tributaries-ml/src/tributaries/Central.py
@@ -27,7 +27,7 @@ def sbatch_deploy(hyperparams, deploy_config):
     os.makedirs(args.logger.path, exist_ok=True)
 
     from tributaries.Sweeps import my_sweep
-    my_sweep.update({'app_name_paths': None, 'commands': [], 'sbatch': ''})
+    my_sweep.update({'app_name_path': None, 'commands': [], 'sbatch': ''})
 
     deploy_config.update({key: value for key, value in my_sweep.items() if key not in deploy_config})
 
