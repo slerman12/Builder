efcc219a75914748c86cbfa74f7fc404dc8f7723
sam <slerman@u.rochester.edu>
2023-07-09 15:37:55 -0400

    Install

--- Install.py
+++ Install.py
@@ -1,14 +1,12 @@
+import glob
 import os
 
 os.system(f'git pull')
 
-# builds = glob.glob('*/Build.py')
-#
-# for build in builds:
-#     os.system(f'python {build}')
+builds = glob.glob('*/dist/*')
 
 with open('install.sh', 'w') as f:
-    f.write("""git pull
-pip install UnifiedML/dist/UnifiedML-1.0.0-py3-none-any.whl tributaries-ml/dist/tributaries_ml-1.0.0-py3-none-any.whl minihydra/dist/minihydra-1.0.0-py3-none-any.whl --force-reinstall --no-dependencies""")
+    f.write(f"""git pull
+pip install {' '.join(builds)} --force-reinstall --no-dependencies""")
 
 os.system('sh install.sh')
