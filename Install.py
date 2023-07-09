f863f4b35809df9a13bb485e07c83376e67e4989
sam <slerman@u.rochester.edu>
2023-07-09 15:38:36 -0400

    Install

--- Install.py
+++ Install.py
@@ -6,7 +6,6 @@ os.system(f'git pull')
 builds = glob.glob('*/dist/*')
 
 with open('install.sh', 'w') as f:
-    f.write(f"""git pull
-pip install {' '.join(builds)} --force-reinstall --no-dependencies""")
+    f.write(f"""pip install {' '.join(builds)} --force-reinstall --no-dependencies""")
 
 os.system('sh install.sh')
