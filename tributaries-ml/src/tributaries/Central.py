36a7acd363f08c520a4ccc14ec0734b2d7fc7832
sam <slerman@u.rochester.edu>
2023-08-02 15:49:43 -0400

    readme

--- tributaries-ml/src/tributaries/Central.py
+++ tributaries-ml/src/tributaries/Central.py
@@ -3,7 +3,7 @@
 # This source code is licensed under the MIT license found in the
 # MIT_LICENSE file in the root directory of this source tree.
 """
-A simple port/mill operating on the branching tributaries of data flow between remote servers, hand-built by Sam Lerman.
+A simple port/mill operating on the branching rivers of data flow between remote servers. Hand-built by Sam Lerman.
 """
 
 import os
