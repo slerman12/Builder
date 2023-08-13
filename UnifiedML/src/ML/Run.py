35be4dd0de6bf8c645e136b8ba0284ada37166bc
sam <slerman@u.rochester.edu>
2023-08-13 14:15:00 -0400

    At least somewhat working draft of everything

--- UnifiedML/src/ML/Run.py
+++ UnifiedML/src/ML/Run.py
@@ -30,7 +30,6 @@ def main(args):
     # Agent
     agent = load(args.load_path, args.device, args.agent) if args.load \
         else instantiate(args.agent).to(args.device)
-
     # replay.set_tape(getattr(agent, 'rewrite_shape', ()))  # TODO Optional rewritable memory
 
     # Synchronize multi-task models (if exist)
