1b73458d4b9036331703858b28267351f1f2ac66
sam <slerman@u.rochester.edu>
2023-10-08 01:35:28 -0400

    Bittle object detector setup debug

--- tributaries-ml/src/tributaries/Examples/Sweeps/Bittle.py
+++ tributaries-ml/src/tributaries/Examples/Sweeps/Bittle.py
@@ -31,7 +31,7 @@ my_sweep.hyperparams = [
     aug=Agents.Blocks.Architectures.Vision.FoundationModels.GroundingDINO.AutoLabel
     aug.caption='little robot dog'
     action_spec.shape='(4,)' 
-    batch_size=16
+    batch_size=1
     evaluate_per_steps=0
     stream=true
     """,
