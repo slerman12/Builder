9b6b643fba0c1c02ebc1665b5afc208bb3ec50fa
sam <slerman@u.rochester.edu>
2023-12-14 15:15:25 -0500

    Pip packaging

---
+++ upload.py
@@ -0,0 +1,27 @@
+import glob
+import os, sys
+
+os.system(f'git pull')
+
+builds = glob.glob('*/build/')
+
+for build in builds:
+    os.system(f'rm -rf {build}')
+
+builds = glob.glob('*/Build.py')
+
+for build in builds:
+    os.system(f'python {build}')
+
+builds = glob.glob('*/dist/*')
+
+if '--from-scratch' in sys.argv:
+    os.system(f'pip install {" ".join(builds)} --force-reinstall')
+else:
+    os.system(f'pip install {" ".join(builds)} --force-reinstall --no-dependencies')
+
+for build in builds:
+    print(f'Uploading {build}')
+    os.system(f'twine upload {build}')
+
+# If issues, may be necessary to delete auto-generated build/ directories and re-run.
