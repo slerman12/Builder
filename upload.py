f196801e8d9c7f5cac911c27f2da2cd2a209cf3f
sam <slerman@u.rochester.edu>
2023-12-14 19:59:25 -0500

    Pip packaging

--- upload.py
+++ upload.py
@@ -11,7 +11,9 @@ builds = glob.glob('*/dist/*')
 # Can change token via account settings on PyPi
 
 for build in builds:
-    print(f'Upload: {build}')
+    print(f'python -m twine upload {build}')
     # os.system(f'python -m twine upload {build}')
+
+print('\nUsername: __token__\nPassword: pypi-AgEIcHlwaS5vcmcCJDkzZDAzMTViLTVlYTUtNDhhNC04YmY0LTY2ODM4NjBiMGU5ZAACKlszLCI5MTE0MzAwMi1iMzI0LTQ4NzYtOTA3Zi0wOGE0MWYzNzM4NTgiXQAABiAVA6NeKfY62XOl9x5aVTtGulT5pJT5L-TxpwPQo5Uyow')
 
 # If issues, may be necessary to delete auto-generated build/ directories and re-run.
