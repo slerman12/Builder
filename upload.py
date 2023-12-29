0f1bdc9c9640dad73237ef0bd93b7908ebffbe17
sam <slerman@u.rochester.edu>
2023-12-28 20:01:56 -0500

    Pip packaging

--- upload.py
+++ upload.py
@@ -16,5 +16,4 @@ for build in builds:
     print(f'python -m twine upload {build}')
 
 print('\nUsername: __token__'
-      '\nPassword: pypi-AgEIcHlwaS5vcmcCJDkzZDAzMTViLTVlYTUtNDhhNC04YmY0LTY2ODM4NjBiMGU5ZAACKlszLCI5MTE0MzAwMi1iMzI0LTQ'
-      '4NzYtOTA3Zi0wOGE0MWYzNzM4NTgiXQAABiAVA6NeKfY62XOl9x5aVTtGulT5pJT5L-TxpwPQo5Uyow')
+      '\nPassword: <see token on PyPi>')
