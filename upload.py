b8d8077b3b5ac16b84b593a26878f4fb563f3808
sam <slerman@u.rochester.edu>
2023-12-28 20:01:39 -0500

    Pip packaging

--- upload.py
+++ upload.py
@@ -5,9 +5,12 @@ import glob
 builds = glob.glob('*/dist/*')
 
 # Username: __token__
-# Password: pypi-AgEIcHlwaS5vcmcCJDkzZDAzMTViLTVlYTUtNDhhNC04YmY0LTY2ODM4NjBiMGU5ZAACKlszLCI5MTE0MzAwMi1iMzI0LTQ4NzYtOTA3Zi0wOGE0MWYzNzM4NTgiXQAABiAVA6NeKfY62XOl9x5aVTtGulT5pJT5L-TxpwPQo5Uyow
+# Password: <see token on PyPi>
 
 # Can change token via account settings on PyPi
+# https://pypi.org/manage/account/#api-tokens
+
+# Also have to iterate version in setup.py, re-build, and delete previous distribution
 
 for build in builds:
     print(f'python -m twine upload {build}')
