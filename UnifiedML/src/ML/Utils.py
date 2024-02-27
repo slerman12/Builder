Debugged. TODOs: (1) Test all learning modes again. (2) Get rid of redundant console-print after convergence. (3) Correct Precision and Recall in Metrics. (4) "Cybernetic phase" - arbitrary exp inputs, arbitrary exp outputs.

UnifiedML/src/ML/Utils.py
@@ -200,7 +200,7 @@ def save(path, model, args=None, *attributes):
 
 
 # Loads model or part of model
-def load(path, device='cuda', args=None, preserve=(), distributed=False, attr='', **kwargs):
+def load(path, device='cuda', args=None, preserve=(), attr='', **kwargs):
     while True:
         try:
             to_load = torch.load(path, map_location=device, pickle_module=dill)  # Load
@@ -217,7 +217,7 @@ def load(path, device='cuda', args=None, preserve=(), distributed=False, attr=''
                 args = original_args  # If already instantiated, use instantiated
             break
         except Exception as e:  # Pytorch's load and save are not atomic transactions, can conflict in distributed setup
-            if not distributed:
+            if not preserve:
                 raise RuntimeError(e)
             warnings.warn(f'Load conflict, resolving...')  # For distributed training
 

UnifiedML/src/ML/World/Environment.py
@@ -81,12 +81,13 @@ class Environment:
             with act_mode(agent, self.ema):
                 action = agent.act(obs, store)  # TODO Allow agent to output an exp
 
-            action = self.infer_action_from_action_spec(action)
+            # Inferred action will get passed to Env, Metrics, and Logger. However, original will be stored in Replay.
+            _action = self.infer_action_from_action_spec(action)
 
             if not self.generate:
-                action = action.cpu().numpy()  # TODO Maybe standardize Env as Tensor
+                _action = _action.cpu().numpy()  # TODO Maybe standardize Env as Tensor
 
-            prev, now = {}, {} if self.generate else self.env.step(action)  # Experience
+            prev, now = {}, {} if self.generate else self.env.step(_action)  # Experience
 
             # The point of prev is to group data by time step since some datums, like reward, are delayed 1 time step
             if isinstance(now, tuple):
@@ -95,21 +96,26 @@ class Environment:
             now = now or {}  # Can be None
 
             self.exp.update(store)
-            self.exp.update(action=now.get('action', action), step=agent.step)
+            self.exp.update(action=now.get('action', _action), step=agent.step)
             self.exp.update(prev)  # Prev is only needed for metric
 
             # Tally reward & logs
             self.tally_metric(self.exp)
 
             if 'reward' not in now and 'reward' in self.exp:
-                now.reward = self.exp.reward  # Metric can actually set reward, e.g. if RL
+                # Note: Envs should either always return a reward or never return a reward.
+                # Inconsistent presence of reward can lead to tally_metric carrying over last-present
+                # reward to next time steps
+                now.reward = self.exp.reward  # Metric can actually set reward, e.g., if RL
+
+            now.action = action
 
             # Transform
             now = self.transform(now)
 
             if agent.training:
                 # These go to Replay and include now (mixed time steps)
-                experiences.append(Args({'action': action, 'step': agent.step, **prev, **now, **store}))
+                experiences.append(Args({'step': agent.step, **prev, **now, **store}))
             else:
                 # These go to logger and don't include now
                 experiences.append(self.exp)
@@ -223,25 +229,28 @@ class Environment:
     Metric tallying and tabulating for inference/evaluation
     """
     # Compute metric on batch
-    def tally_metric(self, exp):
-        # TODO Maybe standardize to Tensors
+    def tally_metric(self, exp):  # TODO Maybe standardize to Tensors
+        # Convert batch to numpy
         for key, value in exp.items():
             if isinstance(value, torch.Tensor):
                 exp[key] = value.cpu().numpy()
 
+        # Compute each function-based (not string-based) metric on the batch. String-based gets computed in tabulate
         add = {key: m.add(exp) for key, m in self.metric.items() if callable(getattr(m, 'add', None))}
 
+        # Add metrics to episode-long running list of metrics
         self.episode_adds.update({key: self.episode_adds.get(key, []) + [m]
                                   for key, m in add.items() if m is not None})
 
         # Always include reward
         if 'reward' in self.metric and 'reward' in self.episode_adds:
             exp.reward = self.episode_adds['reward'][-1]  # Override Env reward with metric reward
-        elif 'reward' in self.metric or 'reward' in exp:
+        elif ('reward' in self.metric or 'reward' in exp) and not getattr(self, 'popped_reward', None):
             # Evaluate reward from string  Note: No recursion between strings
             if isinstance(self.metric.get('reward', None), str):
                 exp.reward = eval(self.metric['reward'], {key: episode[-1]
                                                           for key, episode in self.episode_adds.items()})
+
             # Override metric reward with Env reward
             self.episode_adds.setdefault('reward', []).append(exp.reward.mean() if hasattr(exp.reward, 'mean')
                                                               else exp.reward)
@@ -253,6 +262,9 @@ class Environment:
             warnings.warn(f'"RL" enabled but no Env reward found. Using metric "{key}" as reward. '
                           f'Customize your own reward with the "reward=" flag. For example: '
                           f'"reward=path.to.rewardfunction" or even "reward=-{key}+1". See docs for more demos.')
+
+            # To avoid this becoming the reward used next time in the previous elif block, and then never changing
+            setattr(self, 'popped_reward', True)
 
     # Aggregate metrics
     def tabulate_metric(self):
@@ -300,13 +312,13 @@ class Environment:
 
         discrete_bins, low, high = self.action_spec['discrete_bins'], self.action_spec['low'], self.action_spec['high']
 
-        # TODO Generalize to regression
+        # Round to nearest decimal/int corresponding to discrete bins, high, and low
         if self.action_spec['discrete']:
-            # Round to nearest decimal/int corresponding to discrete bins, high, and low
             action = torch.round((action - low) / (high - low) * (discrete_bins - 1)) / \
                      (discrete_bins - 1) * (high - low) + low
         else:
-            pass  # TODO
+            # TODO Generalize to regression
+            pass
 
         return action
 

UnifiedML/src/ML/World/Environments/Atari.py
@@ -29,18 +29,13 @@ class Atari:
 
     (1) a "step" function, action -> exp
     (2) "reset" function, -> exp
-    (3) "render" function, -> image
-    (4) "episode_done" attribute
-    (5) "obs_spec" attribute which includes:
+    (3) "obs_spec" attribute which includes:
         - "shape", "mean", "stddev", "low", "high" (the last 4 can be None)
-    (6) "action-spec" attribute which includes:
+    (4) "action-spec" attribute which includes:
         - "shape", "discrete_bins" (should be None if not discrete), "low", "high", and "discrete"
-    (7) "exp" attribute containing the latest exp
 
-    Recommended: Discrete environments should have a conversion strategy for adapting continuous actions (e.g. argmax)
-
-    An "exp" (experience) is an Args consisting of "obs", "action" (prior to adapting), "reward", and "label"
-    as numpy arrays with batch dim or None. "reward" is an exception: should be numpy array, can be empty/scalar/batch.
+    An "exp" (experience) is a dict consisting of keys such as "obs", "action", "reward", and "label"
+    as numpy arrays with batch dim or None. "reward" should be numpy array, can be scalar/batch.
 
     ---
 
@@ -106,20 +101,17 @@ class Atari:
                                  'high': self.env.action_space.n - 1,
                                  'discrete': True})
 
-        self.exp = None
-
         self.action_repeat = action_repeat or 1
         self.frames = deque([], frame_stack or 1)
 
     def step(self, action):
-        # Adapt to discrete!
-        _action = self.adapt_to_discrete(action)
-        _action.shape = self.action_spec['shape']
+        # Remove batch dim
+        action = np.reshape(action, self.action_spec['shape'])
 
         # Step env
         reward = np.zeros([])
         for _ in range(self.action_repeat):
-            obs, _reward, self.episode_done, _, _ = self.env.step(int(_action))  # Atari requires scalar int action
+            obs, _reward, self.episode_done, _, _ = self.env.step(int(action))  # Atari requires scalar int action
             reward += _reward
             if self.last_2_frame_pool:
                 last_frame = self.last_frame
@@ -150,13 +142,8 @@ class Atari:
         # Add batch dim
         obs = np.expand_dims(obs, 0)
 
-        # Create experience
-        exp = {'obs': obs, 'action': action, 'reward': reward, 'done': self.episode_done}  # TODO Auto-done
-
-        self.exp = Args(exp)  # Experience
-
-        prev = {'reward': reward, 'action': action}  # Reward for previous action
-        now = {'obs': obs, 'done': self.episode_done}  # New state
+        prev = {'reward': reward}  # Reward for previous action
+        now = {'obs': obs, 'done': self.episode_done}  # New state  # TODO Auto-done
 
         return prev, now
 
@@ -197,27 +184,7 @@ class Atari:
         # Reset frame stack
         self.frames.clear()
 
-        self.exp = Args(exp)  # Experience
-
-        return self.exp
+        return Args(exp)
 
     def render(self):
         return self.env.render()
-
-    def adapt_to_discrete(self, action):
-        shape = self.action_spec['shape']
-
-        try:
-            action = action.reshape(len(action), *shape)  # Assumes a batch dim
-        except (ValueError, RuntimeError):
-            try:
-                action = action.reshape(len(action), -1, *shape)  # Assumes a batch dim
-            except:
-                raise RuntimeError(f'Discrete environment could not broadcast or adapt action of shape {action.shape} '
-                                   f'to expected batch-action shape {(-1, *shape)}')
-            action = action.argmax(1)
-
-        discrete_bins, low, high = self.action_spec['discrete_bins'], self.action_spec['low'], self.action_spec['high']
-
-        # Round to nearest decimal/int corresponding to discrete bins, high, and low
-        return np.round((action - low) / (high - low) * (discrete_bins - 1)) / (discrete_bins - 1) * (high - low) + low

UnifiedML/src/ML/World/Environments/DMC.py
@@ -23,18 +23,13 @@ class DMC:
 
     (1) a "step" function, action -> exp
     (2) "reset" function, -> exp
-    (3) "render" function, -> image
-    (4) "episode_done" attribute
-    (5) "obs_spec" attribute which includes:
+    (3) "obs_spec" attribute which includes:
         - "shape", "mean", "stddev", "low", "high" (the last 4 can be None)
-    (6) "action-spec" attribute which includes:
+    (4) "action-spec" attribute which includes:
         - "shape", "discrete_bins" (should be None if not discrete), "low", "high", and "discrete"
-    (7) "exp" attribute containing the latest exp
 
-    Recommended: Discrete environments should have a conversion strategy for adapting continuous actions (e.g. argmax)
-
-    An "exp" (experience) is an Args consisting of "obs", "action" (prior to adapting), "reward", and "label"
-    as numpy arrays with batch dim or None. "reward" is an exception: should be numpy array, can be empty/scalar/batch.
+    An "exp" (experience) is a dict consisting of keys such as "obs", "action", "reward", and "label"
+    as numpy arrays with batch dim or None. "reward" should be numpy array, can be scalar/batch.
 
     ---
 
@@ -107,13 +102,11 @@ class DMC:
                                  'high': 1,
                                  'discrete': False})
 
-        self.exp = None  # Experience
-
         self.action_repeat = action_repeat
         self.frames = deque([], frame_stack or 1)
 
     def step(self, action):
-        # Remove batch dim, adapt shape
+        # Remove batch dim
         action = np.reshape(action, self.action_spec['shape'])
 
         # Step env
@@ -130,18 +123,13 @@ class DMC:
 
         obs = time_step.observation[self.key].copy()  # DMC returns numpy arrays with negative strides, need to copy
 
-        # Create experience
-        exp = {'obs': obs, 'action': action, 'reward': reward, 'done': self.episode_done}
         # Add batch dim
-        exp['obs'] = np.expand_dims(exp['obs'], 0)
-        exp['action'] = np.expand_dims(exp['action'], 0)
+        obs = np.expand_dims(obs, 0)
         # Channel-first
-        exp['obs'] = exp['obs'].transpose(0, 3, 1, 2)
+        obs = obs.transpose(0, 3, 1, 2)
 
-        self.exp = Args(exp)  # Experience
-
-        prev = {'reward': exp.pop('reward'), 'action': exp.pop('action')}  # Reward for previous action
-        now = exp  # New state
+        prev = {'reward': reward}  # Reward for previous action
+        now = {'obs': obs, 'done': self.episode_done}  # New state
 
         return prev, now
 
@@ -168,9 +156,7 @@ class DMC:
         # Reset frame stack
         self.frames.clear()
 
-        self.exp = Args(exp)  # Experience
-
-        return self.exp
+        return Args(exp)  # Experience
 
     def render(self):
         return self.env.physics.render(height=256, width=256, camera_id=0)