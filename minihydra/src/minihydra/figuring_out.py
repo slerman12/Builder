a90ad38cdc0a1a10623847d3bfc7f6002a2632b8
sam <slerman@u.rochester.edu>
2024-11-14 09:15:35 -0500

    Figuring out steps to finalize minihydra

--- minihydra/src/minihydra/figuring_out.py
+++ minihydra/src/minihydra/figuring_out.py
@@ -236,6 +236,7 @@ class Poo:
 
 """
 1. Convert paths to '/' and include current working directory at start. Each end with '/'
+    $ path.replace(os.sep, '/')
 2. Go through target and pull out known_path and dot_paths. Convert former to '/'. Known path ends with '/'
 3. If known path starts with '/', then treat paths as empty
 4. Iterate through paths, and go through each dot path one by one
